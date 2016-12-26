a0228626472a6f4d793343a7be79dd9dc16e0acb
lynn.zhu <zxl20zxl@gmail.com>
2016-12-26 18:12:25 +0800

    first commit

---
+++ YLFPSLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLFPSLabel
+//
+//  Created by 张晓岚 on 16/12/26.
+//  Copyright © 2016年 ThinkMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YLFPSLabel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YLFPSLabel
+//
+//  Created by 张晓岚 on 16/12/26.
+//  Copyright © 2016年 ThinkMobile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
